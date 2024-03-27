11f3be9e780c4f1c9ce498887656f24da1810b0f
TayyabaIrfan <tayyabairfan098@gmail.com>
2024-03-26 22:20:23 -0700

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() *5+1);
+
+const answers = await inquirer.prompt([
+   { name: "userGuessedNumber",
+      type: "number",
+      message: "Enter a number between 1 and 5"}
+])
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You Guessed Right Number.");
+} else { console.log("Better Luck For Next Time!"); }
